694a48deca2cc4285c5c38aa4e83d5aa822c3997
ratatatatat <rajiv.puri1192@gmail.com>
2017-03-05 20:07:19 -0500

    Some game framework

---
+++ models/game.js
@@ -0,0 +1,19 @@
+//Instantiate a game with rules(config)
+
+//
+//	Set The Rules 
+//	Create Max number of players
+//	The Dealer Stand Rule
+//	The Number of Decks
+//	The Percent Till Deck Gets ReShuffled
+//	Payouts
+//	Double-down
+//	Split Rule
+//	Dealer BlackJack
+
+//	Games
+//	Player Joins Game
+//	Player Places Bets
+//	Game Starts
+//	All actions are taken on Bets
+//	All Accounts are settled
